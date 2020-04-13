a91acb6514db7f455ef863c64e5b8737dd3a89cd
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2020-04-13 13:07:20 +0200

    menu working with an angle

--- Assets/Scripts/UI/UIRadialMenuAction.cs
+++ Assets/Scripts/UI/UIRadialMenuAction.cs
@@ -4,6 +4,9 @@ using TMPro;
 public class UIRadialMenuAction : MonoBehaviour
 {
     [SerializeField]
+    private float cancelUntilDistance = 50f;
+
+    [SerializeField]
     private float mouseSensitivity = 1f;
 
     private Canvas canvas = default;
@@ -34,9 +37,30 @@ public class UIRadialMenuAction : MonoBehaviour
         cursorMenu.transform.localPosition = Vector3.ClampMagnitude(cursorMenu.transform.localPosition, 100);
     }
 
+    private float GetClockwiseAngle()
+    {
+        float angle;
+
+        if (cursorMenu.transform.localPosition.x < 0)
+            angle = 180 + Vector3.Angle(Vector3.down, cursorMenu.transform.localPosition);
+        else
+            angle = Vector3.Angle(Vector3.up, cursorMenu.transform.localPosition);
+
+        return angle;
+    }
+
     public HandActionId GetSelectedAction()
     {
-        return HandActionId.Cancel;
+        if (cursorMenu.transform.localPosition.magnitude < cancelUntilDistance)
+        {
+            return HandActionId.Cancel;
+        }
+
+        int itemsCount = possibleActions.Length;
+        float cursorAngle = GetClockwiseAngle();
+        int selectedItemIndex = Mathf.FloorToInt(cursorAngle / 360f * (possibleActions.Length));
+        Debug.Log("selectedItemIndex=" + selectedItemIndex);
+        return possibleActions[selectedItemIndex];
     }
 
     public void ShowActions(HandActionId[] possibleActions)
@@ -46,15 +70,17 @@ public class UIRadialMenuAction : MonoBehaviour
         foreach (HandActionId actionId in possibleActions)
         {
             GameObject menuItem = Instantiate(menuItemPrefab, canvas.transform);
-            float rotation = (360 / possibleActions.Length) * i;
-            menuItem.transform.Rotate(0, 0, rotation);
+            float itemAngle = (360 / possibleActions.Length);
+            float clockwiseAngle = (itemAngle * -i) - (itemAngle / 2);
+            menuItem.transform.Rotate(0, 0, clockwiseAngle);
             Transform childTextTransform = menuItem.transform.GetChild(0);
-            childTextTransform.Rotate(0, 0, -rotation);
+            childTextTransform.Rotate(0, 0, -clockwiseAngle);
             childTextTransform.GetComponent<TextMeshProUGUI>().text = i.ToString();
             i++;
         }
 
         this.possibleActions = possibleActions;
+        cursorMenu.transform.localPosition = Vector3.zero;
         cursorMenu.SetActive(true);
         canvas.gameObject.SetActive(true);
     }
